78d349c342537f35d831d08ebc569b5d7a3496c6
antorpo <antonio.gonzalezr@udea.edu.co>
2020-03-08 22:51:30 -0500

    Se agrega Componentes: BadgesList y BadgeListItem.

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import "./styles/Badges.css";
 import confLogo from "../images/badge-header.svg";
+import BadgesList from "../components/BadgesList";
 
 class Badges extends React.Component {
     state={
@@ -65,15 +66,7 @@ class Badges extends React.Component {
 
           <div className="Badges__list">
             <div className="Badges__container">
-                <ul className="list-unstyled">
-                    {this.state.data.map((badge) => {
-                        return(
-                            <li key={badge.id}>
-                                {badge.firstName} {badge.lastName} 
-                            </li>
-                        )
-                    })}
-                </ul>
+                <BadgesList badges={this.state.data}/>
             </div>
           </div>
         </div>
